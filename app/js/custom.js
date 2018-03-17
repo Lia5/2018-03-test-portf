8aeaed382da09fd7a38126710a3b22b4e07c8a23
Lia5 <Liaforweb@gmail.com>
2018-03-18 00:27:41 +0200

    menu js

--- app/js/custom.js
+++ app/js/custom.js
@@ -9,3 +9,35 @@ trigger.addEventListener('click', function() {
   };
 });
 
+var item = document.querySelector('.menu__item');
+var item2 = document.querySelector('.menu__item2');
+var item3 = document.querySelector('.menu__item3');
+var item4 = document.querySelector('.menu__item4');
+item.addEventListener('click', function() {
+  if (trigger.classList.contains('trigger--show')) {
+    trigger.classList.remove('trigger--show');
+  } else {
+    trigger.classList.add('trigger--show')
+  };
+});
+item2.addEventListener('click', function() {
+  if (trigger.classList.contains('trigger--show')) {
+    trigger.classList.remove('trigger--show');
+  } else {
+    trigger.classList.add('trigger--show')
+  };
+});
+item3.addEventListener('click', function() {
+  if (trigger.classList.contains('trigger--show')) {
+    trigger.classList.remove('trigger--show');
+  } else {
+    trigger.classList.add('trigger--show')
+  };
+});
+item4.addEventListener('click', function() {
+  if (trigger.classList.contains('trigger--show')) {
+    trigger.classList.remove('trigger--show');
+  } else {
+    trigger.classList.add('trigger--show')
+  };
+});
